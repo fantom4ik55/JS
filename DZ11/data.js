609ea785ce778f874feccd5892802f82d6975408
Лёша Лёша <fantom.rpg@mail.ru>
2024-09-10 15:25:43 +0300

    dz11

---
+++ DZ11/data.js
@@ -0,0 +1,32 @@
+
+
+
+const dataInfo =`
+[
+    {
+      "code": "Россия, Московская обл.",
+      "name": "Москва",
+      "message": "https://avatars.mds.yandex.net/get-entity_search/5528860/919706477/orig",
+      "population": "Население: 13млн. человек"
+    },
+    {
+      "code": "Россиия, Ленинградская обл",
+      "name": "Санкт-Петербург",
+      "message": "https://rg.ru/uploads/images/2023/05/31/istock-1478313878_a0d.jpg",
+      "population": "Население: 5,6млн. человек"
+    },
+    {
+      "code": "Россия",
+      "name": "Севастополь",
+      "message": "https://grandtrain.ru/upload/iblock/a86/zwv1j9xrpz42ilgbo3jwnm7ulqfdlbh1.jpg",
+      "population": "547 тыс. человек"
+    },
+    {
+      "code": "Россия, Ростовская область",
+      "name": "Ростов-на-Дону",
+       "message": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyQIjCVV8ysKnuEebpkROu60Xh_mH-DHPvWA&s",
+      "population": "1,13 млн. человек"
+    }
+    
+  ]
+  `
